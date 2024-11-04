bf8d2f6f9c0c17b47141968631dcae590980a425
Vitor Reis <reis_vitor@outlook.com>
2024-11-04 02:48:25 -0300

    refactor: remove streams vars. feat: add is connect method

--- src/domain/models/Client.java
+++ src/domain/models/Client.java
@@ -1,26 +1,17 @@
 package src.domain.models;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.Socket;
 
 public class Client {
   private String ip;
-  private String serverIp;
-
   private DatagramPacket datagramPacket;
   private Socket socket;
-  private ObjectOutputStream outputStream;
-  private ObjectInputStream inputStream;
 
   public Client(Socket socket) throws IOException {
     this.socket = socket;
     this.ip = socket.getInetAddress().getHostAddress();
-    this.serverIp = socket.getLocalAddress().getHostAddress();
-    this.outputStream = new ObjectOutputStream(socket.getOutputStream());
-    this.inputStream = new ObjectInputStream(socket.getInputStream());
   }
 
   public Client(DatagramPacket datagramPacket) {
@@ -30,11 +21,9 @@ public class Client {
 
   public void close() {
     try {
-      if (outputStream != null)
-        outputStream.close();
-
-      if (inputStream != null)
-        inputStream.close();
+      if (this.socket != null) {
+        this.socket.close();
+      }
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -44,24 +33,16 @@ public class Client {
     return this.socket;
   }
 
+  public Boolean isConnected() {
+    if (this.socket != null) {
+      return this.socket.isConnected() && !this.socket.isClosed();
+    }
+
+    return false;
+  }
+
   public void setSocket(Socket socket) {
     this.socket = socket;
-  }
-
-  public ObjectOutputStream getOutputStream() {
-    return outputStream;
-  }
-
-  public void setOutputStream(ObjectOutputStream outputStream) {
-    this.outputStream = outputStream;
-  }
-
-  public ObjectInputStream getInputStream() {
-    return inputStream;
-  }
-
-  public void setInputStream(ObjectInputStream inputStream) {
-    this.inputStream = inputStream;
   }
 
   public String getIp() {
@@ -70,14 +51,6 @@ public class Client {
 
   public void setIp(String ip) {
     this.ip = ip;
-  }
-
-  public String getServerIp() {
-    return serverIp;
-  }
-
-  public void setServerIp(String serverIp) {
-    this.serverIp = serverIp;
   }
 
   public DatagramPacket getDatagramPacket() {
